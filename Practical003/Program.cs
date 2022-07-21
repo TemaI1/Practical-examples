b3fbb681a20d0b0b67bbb455cbcdb6b30ef5d902
Artem <xTema05x@mail.ru>
2022-07-21 21:51:35 +0300

    practical003 проверяет, является ли первое число квадратом второго

---
+++ Practical003/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = 0;
+int number2 = 0;
+int number3 = 0;
+
+Console.Write("Введите первое число: ");
+number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+number2 = int.Parse(Console.ReadLine());
+
+number3 = number * number;
+
+if(number3 == number2)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
