4f46a6450632e81ac557ac2bdfd337b907216802
Artem <xTema05x@mail.ru>
2022-07-22 11:17:57 +0300

    practical005 выводит максимальное значение из 3 вариантов

---
+++ Practical005/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = 0;
+int b = 0;
+int c = 0;
+int max = 0;
+
+Console.Write("Введите первое число: ");
+a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+c = int.Parse(Console.ReadLine());
+
+if(max > a){
+    max = max;
+}else{
+    max = a;
+}
+
+if(max > b){
+    max = max;
+}else{
+    max = b;
+}
+
+if(max > c){
+    max = max;
+}else{
+    max = c;
+}
+
+
+Console.WriteLine($"Max {max}");
+
