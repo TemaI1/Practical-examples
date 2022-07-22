b039141159cc3a1ac0bd32a4e496f4c1d4b0b585
Artem <xTema05x@mail.ru>
2022-07-22 10:59:11 +0300

    practical004 выводим максимальное и минимальное значения

---
+++ Practical004/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 0;
+int b = 0;
+
+Console.Write("Введите первое число: ");
+a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите первое число: ");
+b = int.Parse(Console.ReadLine());
+
+if (a > b){
+    Console.WriteLine($"Max {a}");
+    Console.WriteLine($"Min {b}");
+}else{
+    Console.WriteLine($"Max {b}");
+    Console.WriteLine($"Min {a}");
+}
