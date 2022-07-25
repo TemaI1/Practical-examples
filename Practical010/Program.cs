200dc2559c5bebeaee8f38867bee9c9c9841cf35
Artem <xTema05x@mail.ru>
2022-07-25 12:47:52 +0300

    practical010 отвечает на вопрос, 1-7 день выходной или нет

---
+++ Practical010/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите цифру от 1 до 7: ");
+int a = int.Parse(Console.ReadLine());
+
+if(a <= 5){
+    Console.WriteLine("No");
+}else if(a <= 7){
+    Console.WriteLine("Yes");
+}else{
+    Console.WriteLine("Введенное число больше 7, прошу выполнить условия!");
+}
