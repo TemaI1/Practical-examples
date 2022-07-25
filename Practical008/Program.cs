cc97e86eabd45a9c70887b29849ec88312d6e1d9
Artem <xTema05x@mail.ru>
2022-07-25 12:44:43 +0300

    practical008 выводит 2 символ, заданного значения

---
+++ Practical008/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трех значное число: ");
+string a = Console.ReadLine();
+char[] number = new char[a.Length]; 
+
+for(int i = 0; i < a.Length; i++){
+    number[i] = a[i];
+    }
+
+Console.WriteLine(number[1]);
