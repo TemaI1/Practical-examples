4fbef2da21f7dd6c807846ceabaa6d4b658bdb11
Artem <xTema05x@mail.ru>
2022-07-25 12:46:16 +0300

    practical009 выводит 3 цифру, заданного значения

---
+++ Practical009/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string a = Console.ReadLine();
+char[] number = new char[a.Length]; 
+
+for(int i = 0; i < a.Length; i++){
+    number[i] = a[i];
+    }
+
+int b = int.Parse(a);
+
+if(b <= 99){
+    Console.WriteLine("Третьей цифры нет ");
+}else{
+    Console.WriteLine(number[2]);
+}
