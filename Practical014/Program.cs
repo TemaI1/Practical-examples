0f37476400b488f6da45a1095ce9b2e1878fc71f
Artem <xTema05x@mail.ru>
2022-07-26 21:58:10 +0300

    practical014 принимает на вход число и выдаёт количество цифр в числе

---
+++ Practical014/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int GetCountNums (int a){
+    int count = 0;
+    for (int i = 1; a != 0; i++){
+        a = a/10;
+        count++;
+    }
+    return count;
+}
+
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе {GetCountNums(b)}");
