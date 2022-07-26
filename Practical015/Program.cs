c2c44986d1cb4e3b0e18761d547b6fc1a0564a2b
Artem <xTema05x@mail.ru>
2022-07-26 21:58:59 +0300

    practical015 принимает на вход число N и выдаёт произведение чисел от 1 до N

---
+++ Practical015/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число N 
+и выдаёт произведение чисел от 1 до N.
+4  -> 24
+5  -> 120
+*/
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = 1;
+
+for (int i = 1; i <= a; i++){
+    b = b * i;
+}
+
+Console.WriteLine(b);
