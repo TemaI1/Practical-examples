a8dab3dff61072840f9c9a49d95a0a7ae048243c
Artem <xTema05x@mail.ru>
2022-07-26 15:01:24 +0300

    practical012 принимает на вход координаты и находит расстояние между ними

---
+++ Practical012/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.WriteLine("Введите х1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите z1: ");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите х2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y2: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+int num = ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) + ((z2-z1)*(z2-z1));
+
+Console.WriteLine(MathF.Sqrt(num));
