9fc7e68bde95d41f1036de7483c71bd55a12f493
Artem <xTema05x@mail.ru>
2022-07-27 21:52:40 +0300

    practical016 принимает на вход два числа (A и B) и возводит число A в натуральную степень B

---
+++ Practical016/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int c = a;
+
+for(int i = 1; i < b; i++){
+   c = c * a;
+}
+
+Console.Write(c);
