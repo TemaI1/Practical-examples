7228d807b1c363fc3d1edcc17d223cc718d4d2f4
Artem <xTema05x@mail.ru>
2022-07-27 21:53:37 +0300

    practical017 принимает на вход число и выдаёт сумму цифр в числе

---
+++ Practical017/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 27: Напишите программу,
+которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int result = 0;
+
+while(a != 0){
+    result = result + a % 10;
+    a = a / 10;
+}
+
+Console.WriteLine($"Сумма цифр: {result}");
