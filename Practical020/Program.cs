a6ebc4584292007f9c8a4fa563f2833e345fd255
Artem <xTema05x@mail.ru>
2022-07-29 15:18:14 +0300

    practical020 находит сумму элементов, стоящих на нечётных позициях [1] и [3]

---
+++ Practical020/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] array = new int[4];
+int sum = 0;
+
+
+for(int i = 0; i < 4; i++){
+    array[i] = new Random().Next(-99, 100);
+    Console.Write($"{array[i]} ");
+
+    if(array[i] == array[1]){
+        sum += array[i];
+    }
+
+    if(array[i] == array[3]){
+        sum += array[i];
+    }
+}
+
+Console.WriteLine($"-> {sum}");
