87496451219ac28c609a53f9be1699aeb2e365a1
Artem <xTema05x@mail.ru>
2022-07-29 15:19:43 +0300

    practical021 найдет разницу между максимальным и минимальным элементов массива

---
+++ Practical021/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+int[] array = new int[5] {3, 7, 22, 2, 78};
+int max = array[0];
+int min = array[0];
+
+for(int i = 0; i < 5; i++){
+    if(array[i] >= max){
+        max = array[i];
+    }
+    if(array[i] <= min){
+        min = array[i];
+    }
+}
+
+Console.WriteLine($"Max = {max} Min = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным: {max - min}");
